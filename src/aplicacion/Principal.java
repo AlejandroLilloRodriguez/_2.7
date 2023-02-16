33f44f23a6cdc9e7f242085b10233c0e374063b1
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 15:59:58 +0100

    Ejercicio7 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.Ejercicio7;
+public class Principal {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Introduzca el numero");
+        int numero=sc.nextInt();
+        System.out.print(+Ejercicio7.sumaPares(numero));
+    }
 }
--- src/dominio/Ejercicio7.java
+++ src/dominio/Ejercicio7.java
@@ -1,2 +1,15 @@
-package dominio;public class Ejercicio7 {
+package dominio;
+
+public class Ejercicio7 {
+    public static int sumaPares(int n){
+        if(n<=1){
+            return 0;
+        }
+        if(n%2!=0){
+            return sumaPares(n-1);
+        }else{
+            return sumaPares(n-2)+n;
+
+        }
+    }
 }
